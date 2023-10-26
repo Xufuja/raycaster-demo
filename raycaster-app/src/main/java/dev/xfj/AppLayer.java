665ef41aa255c35cef57f10bcdf6cef89820c0e5
Xufuja <xufuja@gmail.com>
2023-10-26 22:29:57 +0200

    GL45 -> GL41

--- raycaster-app/src/main/java/dev/xfj/AppLayer.java
+++ raycaster-app/src/main/java/dev/xfj/AppLayer.java
@@ -1,8 +1,6 @@
 package dev.xfj;
 
-import org.lwjgl.opengl.GL45;
-
-import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.opengl.GL41;
 
 public class AppLayer implements Layer {
 
@@ -21,8 +19,8 @@ public class AppLayer implements Layer {
 
     @Override
     public void onAttach() {
-        GL45.glClearColor(0.3f,0.3f,0.3f,0);
-        GL45.glOrtho(0, 1024, 510, 0, -1, 1);
+        GL41.glClearColor(0.3f, 0.3f, 0.3f, 0);
+        GL41.glOrtho(0, 1024, 510, 0, -1, 1);
     }
 
     @Override
@@ -32,7 +30,7 @@ public class AppLayer implements Layer {
 
     @Override
     public void onUpdate(float ts) {
-        GL45.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        GL41.glClear(GL41.GL_COLOR_BUFFER_BIT | GL41.GL_DEPTH_BUFFER_BIT);
         drawMap2D();
     }
 
@@ -40,23 +38,24 @@ public class AppLayer implements Layer {
     public void onUIRender() {
 
     }
+
     private void drawMap2D() {
         int x, y, xo, yo;
         for (y = 0; y < MAP_Y; y++) {
             for (x = 0; x < MAP_X; x++) {
                 if (MAP[y * MAP_X + x] == 1) {
-                    glColor3f(1, 1, 1);
+                    GL41.glColor3f(1, 1, 1);
                 } else {
-                    glColor3f(0, 0, 0);
+                    GL41.glColor3f(0, 0, 0);
                 }
                 xo = x * MAP_S;
                 yo = y * MAP_S;
-                glBegin(GL_QUADS);
-                glVertex2i(0 + xo + 1, 0 + yo + 1);
-                glVertex2i(0 + xo + 1, MAP_S + yo - 1);
-                glVertex2i(MAP_S + xo - 1, MAP_S + yo - 1);
-                glVertex2i(MAP_S + xo - 1, 0 + yo + 1);
-                glEnd();
+                GL41.glBegin(GL41.GL_QUADS);
+                GL41.glVertex2i(0 + xo + 1, 0 + yo + 1);
+                GL41.glVertex2i(0 + xo + 1, MAP_S + yo - 1);
+                GL41.glVertex2i(MAP_S + xo - 1, MAP_S + yo - 1);
+                GL41.glVertex2i(MAP_S + xo - 1, 0 + yo + 1);
+                GL41.glEnd();
             }
         }
     }
